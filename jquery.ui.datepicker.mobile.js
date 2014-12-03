e154d974c517dda6c16b95ebae82d21fce7bba19
gleidsonnunes <gleidsonrnunes@gmail.com>
2014-12-03 09:49:22 -0200

    ggg

--- jquery.ui.datepicker.mobile.js
+++ jquery.ui.datepicker.mobile.js
@@ -1,6 +1,6 @@
 (function($, undefined ) {	
 		$(document).ready(function(){		
-				$( "input[type='date'], input:jqmData(type='date')" ).each(function(){
+				$( "input[type='date']" ).each(function(){
 						$(this).attr('type', 'text');
 						$(this).datepicker({ dateFormat:"dd/mm/yy", showOtherMonths: true,minDate:$(this).attr( "min" ),maxDate:$(this).attr( "max" )});
 					});	
